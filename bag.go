310c151346aea1dea18a9d42310356262a4a44de
Josh Montoya <josh@montoya.io>
2024-07-05 15:47:17 -0700

    initial-pull: clean up implementation and add comments

--- bag.go
+++ bag.go
@@ -1,39 +1,85 @@
 package bag
 
-func New() *Bag {
+import "math"
+
+func New(c Config) *Bag {
 	var b Bag
-	b.ngramSize = 3
+	c.fill()
+	b.c = c
 	b.labels = map[string]Vocabulary{}
+	b.countByLabel = map[string]int{}
 	return &b
 }
 
 type Bag struct {
-	ngramSize int
-	labels    map[string]Vocabulary
+	c Config
+
+	labels map[string]Vocabulary
+
+	// Count of trained documents by label
+	countByLabel map[string]int
+	// Total count of trained documents
+	totalCount int
 }
 
-func (b *Bag) GetSeniment(in string) (labels map[string]int) {
-	ns := toNGrams(in, b.ngramSize)
-	labels = make(map[string]int, len(b.labels))
-	for _, n := range ns {
-		for label, vocab := range b.labels {
-			labels[label] += vocab[n.String()]
-		}
+func (b *Bag) GetSentiment(in string) (r Results) {
+	ns := toNGrams(in, b.c.NGramSize)
+	r = make(Results, len(b.labels))
+
+	for label, vocab := range b.labels {
+		r[label] = b.getProbability(ns, label, vocab)
 	}
 
 	return
 }
 
 func (b *Bag) Train(in, label string) {
-	var target Vocabulary
-	ns := toNGrams(in, b.ngramSize)
-	target, ok := b.labels[label]
-	if !ok {
-		target = make(Vocabulary)
-		b.labels[label] = target
+	// Convert inbound data to a slice of NGrams
+	ns := toNGrams(in, b.c.NGramSize)
+	// Get vocabulary for a provided label, if the vocabulary doesn't exist, it will be created)
+	v := b.getOrCreateVocabulary(label)
+	// Iterate through NGrams
+	for _, n := range ns {
+		// Increment the vocabulary value for the current NGram
+		v[n.String()]++
 	}
 
+	// Increment count of trained documents for the provided label
+	b.countByLabel[label]++
+	// Increment total count of trained documents
+	b.totalCount++
+}
+
+func (b *Bag) getProbability(ns []NGram, label string, vocab Vocabulary) (probability float64) {
+	// Set initial probability value as the prior probability value
+	probability = b.getPriorProbability(label)
+	// Get the current counts by label (to be used by Laplace smoothing during for-loop)
+	countsByLabel := float64(b.countByLabel[label] + len(vocab))
+	// Iterate through NGrams
 	for _, n := range ns {
-		target[n.String()]++
+		// Utilize Laplace smoothing to improve our results when an ngram isn't found within the trained dataset
+		// Likelihood with Laplace smoothing
+		count := float64(vocab[n.String()] + b.c.SmoothingParameter)
+		// Add logarithmic result of count (plus )
+		probability += math.Log(count / countsByLabel)
 	}
+
+	return
+}
+
+func (b *Bag) getPriorProbability(label string) (probability float64) {
+	count := float64(b.countByLabel[label])
+	total := float64(b.totalCount)
+	return math.Log(count / total)
+}
+
+func (b *Bag) getOrCreateVocabulary(label string) (v Vocabulary) {
+	var ok bool
+	v, ok = b.labels[label]
+	if !ok {
+		v = make(Vocabulary)
+		b.labels[label] = v
+	}
+
+	return
 }
