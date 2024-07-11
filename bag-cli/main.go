9c6cd31d2cd9b095ee2093ee048d7b6be9f6e154
Josh Montoya <josh@montoya.io>
2024-07-11 14:37:31 -0700

    clean up and organize code

--- bag-cli/main.go
+++ bag-cli/main.go
@@ -1,56 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/BurntSushi/toml"
 	"github.com/itsmontoya/bag"
 )
 
-type app struct {
-	trainingSetLocation string
-	interactive         bool
-
-	b *bag.Bag
-
-	lines <-chan string
-	done  <-chan os.Signal
-}
-
-func (a *app) interactivePrint(vals ...any) {
-	if a.interactive {
-		fmt.Print(vals...)
-	}
-}
-
-func (a *app) process() (done bool) {
-	select {
-	case <-a.done:
-		return true
-	case line := <-a.lines:
-		results := a.b.GetResults(line)
-		a.interactivePrint("> ")
-		fmt.Print(results.GetHighestProbability())
-	}
-
-	a.interactivePrint("\n")
-	return !a.interactive
-}
-
 func main() {
 	var a app
+	a.lines = onLine()
+	a.done = onExit()
 	flag.StringVar(&a.trainingSetLocation, "training", "", "./path/to/training.toml")
 	flag.BoolVar(&a.interactive, "interactive", false, "true for interactive mode, false to exit immediately after first result")
 	flag.Parse()
-
-	a.lines = onLine()
-	a.done = onExit()
 
 	var t bag.TrainingSet
 	if _, err := toml.DecodeFile(a.trainingSetLocation, &t); err != nil {
@@ -67,31 +31,4 @@ func main() {
 	for !done {
 		done = a.process()
 	}
-}
-
-func onLine() <-chan string {
-	s := bufio.NewScanner(os.Stdin)
-	lines := make(chan string)
-	go func() {
-		for {
-			if !s.Scan() {
-				return
-			}
-
-			input := string(s.Bytes())
-			lines <- input
-		}
-	}()
-
-	return lines
-}
-
-func onExit() <-chan os.Signal {
-	out := make(chan os.Signal, 1)
-	signal.Notify(out,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
-	return out
 }
