527874d0d56f2213080e1ce657b7ef00a80a1f17
Josh Montoya <josh@montoya.io>
2024-07-18 13:43:39 -0700

    implement makeTrainingSetFromFile

--- trainingset.go
+++ trainingset.go
@@ -1,5 +1,24 @@
 package bag
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-yaml/yaml"
+)
+
+func makeTrainingSetFromFile(filepath string) (t TrainingSet, err error) {
+	var f *os.File
+	if f, err = os.Open(filepath); err != nil {
+		err = fmt.Errorf("error opening training set: %v", err)
+		return
+	}
+	defer f.Close()
+
+	err = yaml.NewDecoder(f).Decode(&t)
+	return
+}
+
 type TrainingSet struct {
 	Config `yaml:"config"`
 
